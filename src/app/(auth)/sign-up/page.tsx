a1bfa3be19efcf626aee57ce644ba29cb9e004d3
gyhmac <gyeonho@gmail.com>
2025-06-12 15:36:40 +0900

    feat: reintroduce SignIn layout and page components with updated structure

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Hello Sign Up Page
+      <p>Sign Up Form will be here</p>
+    </div>
+  )
+}
+
+export default page
