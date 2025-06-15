b4c23bf9704c0a8806b8e4d5acf77934c6cd2654
gyhmac <gyeonho@gmail.com>
2025-06-15 20:45:20 +0900

    feat: add login button and Google login option to SignInCard component

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { DottedSeparator } from "@/components/dotted-separator"
+import { Button } from "@/components/ui/button"
 import { 
     Card, 
     CardContent, 
@@ -42,7 +43,21 @@ export const SignInCard = () => {
                     min = {8}
                     max = {256}
                     />
+                    <Button disabled={false} size="lg" className="w-full">
+                        Login
+                    </Button>
                 </form>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7 flex flex-col gap-y-4">
+                <Button
+                variant="secondary"
+                size="lg"
+                className="w-full">
+                    Login with Google
+                </Button>
             </CardContent>
         </ Card >
     )
